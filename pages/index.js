be75c19171c6e9a77703b002c6d95fb94c590a92
eduardobazler <edubazler@gmail.com>
2023-11-18 19:38:41 -0300

    adicionando página `em contrução`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
 function Home() {
-  return <h1>Vamos para Paris!</h1>;
+  return ( 
+  <div>
+    <h1>Projeto Caminho Suave</h1>
+
+    <div>
+      <p>
+        Esse projeto foi inspirado para ajudar minha imrãzinha nos estudos!
+      </p>
+      <p>
+        O Projeto consiste em criar um site onde eu/usuario possa adicionar cards de pergunta com a respctiva resposta para validar determinado conhecimento.
+        <br />
+        Sei que já existem soluções para isso, mas a ideia e criar dados como tempo que demorei pra responder as questões, quais foram as que eu mais errei ou acertei
+        <br />
+        Para que eu consiga validar quais são os pontos que preciso focar no aprendiozado da minha irmã.
+      </p>
+    </div>
+  </div>
+  );
 }
 
 export default Home;
