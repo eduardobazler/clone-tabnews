ccf5fa3671968ab6bf817bbca26901f4ee5ab39f
eduardobazler <edubazler@gmail.com>
2024-07-24 22:24:44 -0300

    iniciando api status com db

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,33 @@
+import { version } from 'react';
 import database from '/infra/database.js';
 
 async function status(request, response) {
-    const result = await database.query('SELECT 1 + 1 AS SUM');
+    const updated_at = new Date().toISOString('pt-br');
 
-    console.log(result.rows);
+    const databaseVersionResult = await database.query("SHOW server_version;")
+
+    const databaseVersion = parseInt(databaseVersionResult.rows[0].server_version);
+
+    const databaseMaxConectionsResult = await database.query("SHOW max_connections;");
+
+    const databaseMaxConcectionsValue = parseInt(databaseMaxConectionsResult.rows[0].max_connections);
+
+    const databaseOpenedConnetionsResult = await database.query("SELECT COUNT(*) AS opened_connections FROM pg_stat_activity WHERE datname = 'local_db';")
+
+    const databaseOpenedConnetionsValue = parseInt(databaseOpenedConnetionsResult.rows[0].opened_connections);
+
     response
     .status(200)
-    .json({ "response" : "deu certo ?" })
+    .json({
+        updated_at,
+        dependencies:{
+            database:{
+                version: databaseVersion,
+                max_connections: databaseMaxConcectionsValue,
+                opened_connections: databaseOpenedConnetionsValue
+            }
+        }
+    })
 }
 
 export default status;
