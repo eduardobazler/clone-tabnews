96a8cc6ff9d2e10198b6a9e1bca37119ef46f169
eduardobazler <edubazler@gmail.com>
2024-08-16 10:54:23 -0300

    adds `api/v1/migrations` endpont

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,29 @@
+import { cleanDatabase } from "../../../../utils/database.js";
+
+beforeAll(cleanDatabase);
+
+test("POST to /api/v1/migrations should return 201", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST"
+    });
+
+    expect(response.status).toBe(201)
+    
+    const responseBody = await response.json();
+
+    expect(Array.isArray(responseBody)).toBe(true)
+    expect(responseBody.length).toBeGreaterThan(0); 
+})
+
+test("POST to /api/v1/migrations should return 200 in second time return empty array", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST"
+    });
+
+    expect(response.status).toBe(200)
+    
+    const responseBody = await response.json();
+    
+    expect(Array.isArray(responseBody)).toBe(true)
+    expect(responseBody.length).toBe(0);
+})
