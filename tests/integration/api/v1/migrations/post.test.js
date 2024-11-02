8133a0edf028065d850e4c78b35f7a18993dafc1
eduardobazler <edubazler@gmail.com>
2024-11-02 15:40:47 -0300

    fix: adjust prettier lint

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -10,30 +10,36 @@ describe("POST /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pendind migrations", () => {
       test("For the first time", async () => {
-        const response = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
-      
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
         expect(response.status).toBe(201);
-      
+
         const responseBody = await response.json();
-      
+
         expect(Array.isArray(responseBody)).toBe(true);
         expect(responseBody.length).toBeGreaterThan(0);
       });
 
       test("or the second time", async () => {
-        const response = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
-      
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
         expect(response.status).toBe(200);
-      
+
         const responseBody = await response.json();
-      
+
         expect(Array.isArray(responseBody)).toBe(true);
         expect(responseBody.length).toBe(0);
       });
-    })
-  })
-})
+    });
+  });
+});
